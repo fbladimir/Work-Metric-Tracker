e56cbe22dc432f8015fa8bb72dc6f560e7de13d0
fbladimir <franklinbladimirck@yahoo.com>
2022-11-24 19:30:08 -0500

    new folder for all operations

---
+++ Work-Metric-Tracker/Pages/Worker/Operations/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Work_Metric_Tracker.Pages.Worker.Operations
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
